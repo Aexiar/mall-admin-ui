5d6524ac1294e3c47ea486d9e0eb41f9d24cec35
许大仙 <1900919313@qq.com>
2022-09-22 22:22:23 +0800

    🎨: 登录接口页面完成

--- src/api/ums/admin/index.ts
+++ src/api/ums/admin/index.ts
@@ -1,7 +1,7 @@
 import {AdminLoginType, AminLoginReturnType} from "@/types/ums/admin";
-import request from '@/utils/request'
+import request from "@/utils/request";
 
 // 登录的方法
-export const adminLoginAPI:(data:AdminLoginType) => PromiseResult<AminLoginReturnType> = (data) => {
+export const adminLoginAPI: (data: AdminLoginType) => PromiseResult<AminLoginReturnType> = (data) => {
   return request.post("/ums/admin/doLogin", data);
 };
